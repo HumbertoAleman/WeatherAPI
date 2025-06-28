edfcd57f3d4664ca685b358ad27f57145597c4b4
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-28 17:54:40 -0400

    feat: Created earthquake DELETE behaviour

--- src/earthquake/earthquake.delete.js
+++ src/earthquake/earthquake.delete.js
@@ -1,3 +1,25 @@
-export default function earthquakeDelete() {
-	res.send('Not implemented yet');
-}
+import Earthquake from './earthquake.schema.js'
+
+export default async function earthquakeDelete(req, res) {
+	const id = req.params.id
+	if (!/^sismo_\d+$/.test(id)) {
+		return res.status(400)
+			.type('json')
+			.send({ message: 'Could not create record, incorrect id format, HINT: Correct format sismo_{{number}}' })
+	}
+
+	let foundEarthquake
+	try {
+		foundEarthquake = await Earthquake.findOneAndDelete({ id });
+		if ((foundEarthquake ?? undefined) === undefined)
+			return res.status(204).send()
+	} catch (e) {
+		return res.status(400)
+			.type('json')
+			.send({ message: e })
+	}
+
+	return res.status(200)
+		.type('json')
+		.send(foundEarthquake)
+} 
