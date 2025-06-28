85f038949f15bc65d1b61acddac2e2219f7bc5e6
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-28 17:34:02 -0400

    feat: Created test for earthquake POST

--- test/earthquake/earthquake.post.test.js
+++ test/earthquake/earthquake.post.test.js
@@ -13,3 +13,26 @@ afterAll(async () => {
 	await mongoose.connection.close();
 });
 
+describe('POST /earthquakes', () => {
+	it('Should create an earthquake record', async () => {
+		const newItem = {
+			id: "sismo_1",
+			magnitude: 5.4,
+			depth: 30,
+			location: "Chile",
+			date: "2023-11-15"
+		};
+
+		const response = await request(app)
+			.post('/earthquakes')
+			.send(newItem)
+			.expect(201)
+
+		expect(response.body).toHaveProperty('_id');
+		expect(response.body.id).toBe(newItem.id);
+		expect(response.body.magnitude).toBe(newItem.magnitude);
+		expect(response.body.depth).toBe(newItem.depth);
+		expect(response.body.location).toBe(newItem.location);
+		expect(new Date(response.body.date).toISOString()).toBe(new Date(newItem.date).toISOString());
+	})
+})
