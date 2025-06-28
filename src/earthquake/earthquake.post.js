a05ee5b86c409c082b2069735381b453c9a61f43
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-06-28 17:37:50 -0400

    feat: Created earthquake POST feature

--- src/earthquake/earthquake.post.js
+++ src/earthquake/earthquake.post.js
@@ -1,3 +1,25 @@
-export default function earthquakePost(req, res) {
-	res.send('Not implemented yet');
+import Earthquake from './earthquake.schema.js'
+
+export default async function earthquakePost(req, res) {
+	const body = req.body
+	if (!/^sismo_\d+$/.test(body.id)) {
+		return res.status(400)
+			.type('json')
+			.send({ message: 'Could not create record, incorrect id format, HINT: Correct format sismo_{{number}}' })
+	}
+
+	const newEarthquake = new Earthquake({ ...body })
+
+	let savedEarthquake
+	try {
+		savedEarthquake = await newEarthquake.save()
+	} catch (e) {
+		return res.status(400)
+			.type("json")
+			.send({ message: e })
+	}
+
+	return res.status(201)
+		.type("json")
+		.send(JSON.stringify(savedEarthquake))
 }
