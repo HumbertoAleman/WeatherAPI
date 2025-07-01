07f6dca7f7366d9b33c093f7cadb6769588dc953
CrisUni <cecarnevali.23@est.ucab.edu.ve>
2025-07-01 19:00:02 -0400

    feat: Create delete weather

--- src/weather/weather.delete.js
+++ src/weather/weather.delete.js
@@ -1,4 +1,79 @@
-export default function weatherDelete(req, res) {
-	res.send('Not implemented yet');
-}
+import Weather from './weather.schema.js'
 
+/**
+ * @swagger
+ * /weathers:
+ *   delete:
+ *     summary: Delete an weather record
+ *     description: Deletes an weather record by its ID. The ID must follow the format `clima_{{number}}`.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the weather record to delete.
+ *         schema:
+ *           type: string
+ *           pattern: '^sismo_\\d+$'
+ *     responses:
+ *       '200':
+ *         description: Successfully deleted the weather record.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 id:
+ *                   type: string
+ *                   example: "clima-65536"
+ *                 city:
+ *                   type: string
+ *                   example: "Caracas"
+ *                 temperature:
+ *                   type: number
+ *                   format: float
+ *                   example: 8.2
+ *                 humidity:
+ *                   type: number
+ *                   format: float
+ *                   example: 12.5
+ *                 condition:
+ *                   type: string
+ *                   example: "Nublado"
+ *       '204':
+ *         description: No content. The weather record was not found.
+ *       '400':
+ *         description: Bad request. The ID format is incorrect or an error occurred.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Could not create record, incorrect id format, HINT: Correct format clima_{{number}}"
+ */
+export default async function weatherDelete(req, res) {
+	const id = req.params.id
+	if (!/^clima_\d+$/.test(id)) {
+		return res.status(400)
+			.type('json')
+			.send({ message: 'Could not create record, incorrect id format, HINT: Correct format clima_{{number}}' })
+	}
+
+	let foundWeather
+	try {
+		foundWeather = await Weather.findOneAndDelete({ id });
+		if ((foundWeather ?? undefined) === undefined)
+			return res.status(204).send()
+	} catch (e) {
+		return res.status(400)
+			.type('json')
+			.send({ message: e })
+	}
+
+	return res.status(200)
+		.type('json')
+		.send(foundWeather)
+} 
--- test/weather/weather.delete.test.js
+++ test/weather/weather.delete.test.js
@@ -3,40 +3,45 @@ import app from '../../src/index.js'
 import request from 'supertest'
 import mongoose from 'mongoose'
 
-const sampleItem = {
-    id: "clima_1",
-    city: "Caracas",
-    temperature: 30.7,
-    humidity: 82,
-    condition: "Soleado"
-};
-
-beforeAll(async () => {
-	await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-
-	await request(app)
-		.post('/weather')
-		.send(sampleItem)
-		.expect(201)
-});
-
-afterAll(async () => {
-	await mongoose.connection.dropDatabase();
-	await mongoose.connection.close();
-});
-
 describe('DELETE /weather/:id', () => {
+
+    const sampleItem = {
+        id: "clima_1",
+        city: "Caracas",
+        temperature: 30.7,
+        humidity: 82,
+        condition: "Soleado"
+    };
+    
+    beforeAll(async () => {
+        await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+      
+    });
+    
+    beforeEach(async () => {
+        await mongoose.connection.dropDatabase();
+    })
+
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+    
 	it('Should delete an weather record', async () => {
-		const response = await request(app)
+		await request(app)
+            .post('/weather')
+            .send(sampleItem)
+            .expect(201)
+
+        const response = await request(app)
 			.delete(`/weather/${sampleItem.id}`)
 			.expect(200)
 
             expect(response.body).toHaveProperty('_id');
-            expect(response.body.id).toBe(newItem.id);
-            expect(response.body.city).toBe(newItem.city);
-            expect(response.body.temperature).toBe(newItem.temperature);
-            expect(response.body.humidity).toBe(newItem.humidity);
-            expect(response.body.condition).toBe(newItem.condition);
+            expect(response.body.id).toBe(sampleItem.id);
+            expect(response.body.city).toBe(sampleItem.city);
+            expect(response.body.temperature).toBe(sampleItem.temperature);
+            expect(response.body.humidity).toBe(sampleItem.humidity);
+            expect(response.body.condition).toBe(sampleItem.condition);
 	})
 
 	it('Should return 204 when no content is found', async () => {
