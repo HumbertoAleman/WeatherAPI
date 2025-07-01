e3170651598d9347dc8432cfac2e5771f36a7eb2
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-07-01 17:04:33 -0400

    fix: Simplified duplicate ID test

--- test/earthquake/earthquake.post.test.js
+++ test/earthquake/earthquake.post.test.js
@@ -108,17 +108,9 @@ describe('POST /earthquakes', () => {
 
 	it('Should return 400 for repeated ids', async () => {
 		const newItem = { id: "sismo_1", magnitude: 5.4, depth: 30, location: "Chile", date: "2023-11-15" };
-		const response = await request(app)
+		await request(app)
 			.post('/earthquakes')
 			.send(newItem)
-			.expect(201)
-		expect(response.body).toHaveProperty('_id');
-		expect(response.body.id).toBe(newItem.id);
-		expect(response.body.magnitude).toBe(newItem.magnitude);
-		expect(response.body.depth).toBe(newItem.depth);
-		expect(response.body.location).toBe(newItem.location);
-		expect(new Date(response.body.date).toISOString()).toBe(new Date(newItem.date).toISOString());
-
 		const identicalItem = { id: "sismo_1", magnitude: 5.4, depth: 30, location: "Chile", date: "2023-11-15" };
 		const newResponse = await request(app)
 			.post('/earthquakes')
