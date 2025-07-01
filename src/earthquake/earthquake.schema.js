886cce6f809e9f845b3f42a89a99cc625e2c9d9a
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2025-07-01 16:27:44 -0400

    feat: Modified magnitude, depth to guarantee positive numbers

--- src/earthquake/earthquake.schema.js
+++ src/earthquake/earthquake.schema.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 const earthquakeSchema = new mongoose.Schema({
 	id: { type: String, required: true },
-	magnitude: { type: Number, required: true },
-	depth: { type: Number, required: true },
+	magnitude: { type: Number, required: true, min: [0, 'Magnitude cannot be a negative number, got {VALUE}'] },
+	depth: { type: Number, required: true, min: [0, 'Depth cannot be a negative number, got {VALUE}'] },
 	location: { type: String, required: true },
 	date: { type: Date, required: true },
 });
